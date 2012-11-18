f3606705799814b85eea931f4eb1184b97b179a4
Stefano Bonetta <bonnyfone@gmail.com>
2012-11-18 02:18:44 +0100

    Camera Monitor and BayesAnal

--- CameraMonitor/src/com/megadevs/nostradamus/cameramonitor/CameraMonitor.java
+++ CameraMonitor/src/com/megadevs/nostradamus/cameramonitor/CameraMonitor.java
@@ -14,21 +14,21 @@ import com.megadevs.nostradamus.cameramonitor.json.Faces;
 import com.megadevs.nostradamus.cameramonitor.reko.FaceAndSceneRecognitionProvidedByReKognitioncom;
 
 public class CameraMonitor {
+	public static boolean STOP = true;
+	private ArrayList<IPCamera> cameras;
+	private String cacheDir;
+	private int interval=10000;
+	private int timeoutImages=15000;
 
-    private ArrayList<IPCamera> cameras;
-    private String cacheDir;
-    private int interval=10000;
-    private int timeoutImages=15000;
-    
-    public CameraMonitor(){
-    	cameras = new ArrayList<IPCamera>();
-    }
-    
-    
-    
-    public void addCamera(IPCamera cam){
-    	cameras.add(cam);
-    }
+	public CameraMonitor(){
+		cameras = new ArrayList<IPCamera>();
+	}
+
+
+
+	public void addCamera(IPCamera cam){
+		cameras.add(cam);
+	}
 
 
 	public String getCacheDir() {
@@ -42,55 +42,88 @@ public class CameraMonitor {
 		if(!f.exists())
 			f.mkdirs();
 	}
-	
+
+	public void stopMonitor(){
+		STOP=true;
+	}
 
 	public void startMonitor(){
-		for(IPCamera cam : cameras){
-			//Check if cam is reachable
-			System.out.println("Checking cam: "+cam.getId());
-			boolean checkRemoteImageTimed = Utils.checkRemoteImageTimed(cam.getHost(), timeoutImages);
-			if(checkRemoteImageTimed){
-				
-				//save snap
-				System.out.println("--Saving snapshot...");
-				File snap = new File(getCacheDir()+cam.getId()+"_"+System.currentTimeMillis()+".jpg");
-				FileOutputStream fo;
-				try {
-					fo = new FileOutputStream(snap);
-					fo.write(cam.getFrame());
-					fo.close();
+		if(!STOP)return;
 
-				
-				//push to postImage
-				System.out.println("--Pushing...");
-				String uploadImage = PostimageClient.uploadImage(snap);
-				System.out.println(uploadImage);
-				
-				//Call Face detection API
-				FaceAndSceneRecognitionProvidedByReKognitioncom client = new FaceAndSceneRecognitionProvidedByReKognitioncom("PUBmYQQbHcF7g4ZUK3h@yqbvABGScWMr", "PRIeQvOOX3MMDPux@sDd#yzq%P-khu4Z");
-				// A sample method call. These parameters are not properly filled in.
-				// See FaceAndSceneRecognitionProvidedByReKognitioncom.java to find all method names and parameters.
-				
-				System.out.println("--FaceDetection...");
-				String[] rekoApi = APITable.getRekoApi();
-				MashapeResponse<JSONArray> response = client.face(rekoApi[0],rekoApi[1],"face","CameraMonitor",uploadImage,null,null,null);
-				//public MashapeResponse<JSONArray> face(String api_key, String api_secret, String jobs, String name_space, String urls, String user_id, String access_token, String fb_id)
-				Gson g = new Gson();
-				Faces f = g.fromJson(Utils.readInputStreamAsString(response.getRawBody()),Faces.class);
-				System.out.println("Complete.");
-				//now you can do something with the response.
-				System.out.println("API Call returned a response code of " + response.getCode());
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-				} catch (IOException e) {
-					e.printStackTrace();
+		STOP = false;
+		Thread t=new Thread(){
+			public void run(){
+				while(!STOP){
+					for(IPCamera cam : cameras){
+						if(STOP)return;
+						//Check if cam is reachable
+						System.out.println("Checking cam: "+cam.getId());
+						boolean checkRemoteImageTimed = Utils.checkRemoteImageTimed(cam.getHost(), timeoutImages);
+						if(checkRemoteImageTimed){
+							if(STOP)return;
+							//save snap
+							System.out.println("--Saving snapshot...");
+							File snap = new File(getCacheDir()+cam.getId()+"_"+System.currentTimeMillis()+".jpg");
+							FileOutputStream fo;
+							try {
+								fo = new FileOutputStream(snap);
+								fo.write(cam.getFrame());
+								fo.close();
+								if(STOP)return;
+
+								//push to postImage
+								System.out.println("--Pushing...");
+								String uploadImage = PostimageClient.uploadImage(snap);
+								System.out.println(uploadImage);
+								if(STOP)return;
+
+								//Call Face detection API
+								FaceAndSceneRecognitionProvidedByReKognitioncom client = new FaceAndSceneRecognitionProvidedByReKognitioncom("PUBmYQQbHcF7g4ZUK3h@yqbvABGScWMr", "PRIeQvOOX3MMDPux@sDd#yzq%P-khu4Z");
+								// A sample method call. These parameters are not properly filled in.
+								// See FaceAndSceneRecognitionProvidedByReKognitioncom.java to find all method names and parameters.
+
+								System.out.println("--FaceDetection...");
+								String[] rekoApi = APITable.getRekoApi();
+								MashapeResponse<JSONArray> response = client.face(rekoApi[0],rekoApi[1],"face","CameraMonitor",uploadImage,null,null,null);
+								//public MashapeResponse<JSONArray> face(String api_key, String api_secret, String jobs, String name_space, String urls, String user_id, String access_token, String fb_id)
+								//System.out.println(response.getBody());
+								if(STOP)return;
+
+								if(response.getCode()==200){
+									Gson g = new Gson();
+									Faces f = g.fromJson(Utils.readInputStreamAsString(response.getRawBody()),Faces.class);
+									int howMany = f.getFace_detection().size();
+									
+									//TODO scrivere su db
+									System.out.println("Numero di persone: "+howMany);
+									System.out.println("Complete.");
+								}else{
+									System.out.println("Errore API");
+								}
+							} catch (FileNotFoundException e) {
+								e.printStackTrace();
+							} catch (IOException e) {
+								e.printStackTrace();
+							}
+						}
+
+						else{
+							//TODO notificare camera non raggiungibile
+						}
+					}
+					
+					try {
+						System.out.println("Sleeping for "+interval/1000+"s");
+						sleep(interval);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
 				}
 			}
-			
-			else{
-				//TODO notificare camera non raggiungibile
-			}
-		}
+
+		};
+		t.start();
+
 	}
 
 	public int getInterval() {
